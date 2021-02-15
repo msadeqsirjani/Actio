1184460c71f79b589efd759b5c43c228fe833e35
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2021-02-15 22:20:14 +0330

    Creating Domain Models

---
+++ src/Actio.Services.Activities/Domain/Models/Activity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Actio.Services.Activities.Domain.Models
+{
+    public class Activity
+    {
+        public Guid Id { get; protected set; }
+        public string Name { get; protected set; }
+        public string Category { get; protected set; }
+        public string Description { get; protected set; }
+        public Guid UserId { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+
+        protected Activity()
+        {
+
+        }
+
+        public Activity(Guid id, string name, Category category, string description, Guid userId, DateTime createdAt)
+        {
+            Id = id;
+            Name = name;
+            Category = category.Name;
+            Description = description;
+            UserId = userId;
+            CreatedAt = createdAt;
+        }
+    }
+}
---
+++ src/Actio.Services.Activities/Domain/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Actio.Services.Activities.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Actio.Services.Activities.Domain.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> GetAsync(string name);
+        Task<IEnumerable<Category>> BrowseAsync();
+        Task AddAsync(Category category);
+    }
+}
