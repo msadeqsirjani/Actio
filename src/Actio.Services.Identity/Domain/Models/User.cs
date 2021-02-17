76a28fe1a534632370613d784d9502255a27e573
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2021-02-17 10:03:58 +0330

    Defining User Entity

---
+++ src/Actio.Services.Identity/Domain/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using Actio.Common.Exceptions;
+using System;
+
+namespace Actio.Services.Identity.Domain.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public User(string email, string name)
+        {
+            if (email.IsNullOrWhiteSpace())
+                throw new ActioException("empty_user_email", "User email can not be empty");
+
+            if (email.IsValidEmail())
+                throw new ActioException("invalid_user_email", "User email was not valid");
+
+            if (name.IsNullOrWhiteSpace())
+                throw new ActioException("empty_user_name", "User name can not be empty");
+
+            Id = Guid.NewGuid();
+            Email = email;
+            Name = name;
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
